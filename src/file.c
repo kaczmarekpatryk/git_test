4832586033f9323f7ae4b9346c8500fb75a9c59a
Jan Nowak <jannowak@example.com>
2018-04-25 19:12:27 +0200

    komit z aaa

---
+++ src/file.c
@@ -0,0 +1,13 @@
+// Kaczmarek Kacper main.cpp
+// Testing Fifo template and own class "LetterNumber"
+// NODE.h FIFO.h LETTERNUMBER.h required
+
+#include <iostream>
+                               
+using namespace std;
+
+int main()
+{
+    cout << "aaaaaaaaaaaaa aaaa" << endl;
+    return 0;
+}
